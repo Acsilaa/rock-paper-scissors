d56b14a9e45205e26c24d86ae2c075c0e23ebdef
Acsilaa <galeengames@gmail.com>
2023-02-28 09:29:57 +0100

    select done

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 async function delay(t){
     return new Promise(res => setTimeout(res, t));
 }
-
+let canClick = true;
 moves = {
-    rock,
-    paper,
-    scissors,
+    rock : "rock",
+    paper : "paper",
+    scissors : "scissors",
+}
+divs = {
+    rock : "#rock",
+    paper : "#paper",
+    scissors : "#scissors", 
 }
 async function RegisterClick(move){
-    if(move == null) return;
+    if(move == null || !canClick) return;
+    canClick = false;
+    for(let i = 0; i < Object.values(divs).length; i++){
+        $(Object.values(divs)[i]).css('background-color', 'var(--disabled-color)');
+    }
+    $(divs[move]).css('background-color', 'var(--select-color)');
+
+    await delay(900);
+    for(let i = 0; i < Object.values(divs).length; i++){
+        $(Object.values(divs)[i]).css('background-color', 'var(--default-color)');
+    }
+    canClick = true;
 }
 
 $('#rock').click(()=>{
